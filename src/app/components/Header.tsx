42122c63f97aa306e024b4639271a4ca75dac264
daniel <daniel@daniels-MacBook-Pro.local>
2023-05-30 15:28:28 -0500

    initial setup

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface HeaderProps {}
+
+const Header: FC<HeaderProps> = ({}) => {
+	return <div className="w-full bg-slate-600 h-36"></div>;
+};
+
+export default Header;
